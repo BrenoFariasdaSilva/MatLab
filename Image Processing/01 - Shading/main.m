ea63766f0bbb5e043de6f439de439ccce5d5a36b
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-03-25 13:47:09 -0300

    REFACTOR: Updating the Filepaths for the Shading Image Processing Operation

--- Image Processing/01 - Shading/main.m
+++ Image Processing/01 - Shading/main.m
@@ -1,5 +1,5 @@
-shaded_image = imread("/Users/brenofarias/Documents/My Files/Backup/GitHub/Public/MatLab/Image Processing/01 - Shading/Dataset/tungsten_filament_shaded.tif");
-shading_image = imread("/Users/brenofarias/Documents/My Files/Backup/GitHub/Public/MatLab/Image Processing/01 - Shading/Dataset/tungsten_sensor_shading.tif");
+shaded_image = imread("Dataset/tungsten_filament_shaded.tif");
+shading_image = imread("Dataset/tungsten_sensor_shading.tif");
 
 double_shaded_image = im2double(shaded_image);
 double_shading_image = im2double(shading_image);
@@ -8,7 +8,7 @@ result = double(shaded_image) ./ double(shading_image);
 
 imshow(result)
 
-imwrite(result, "/Users/brenofarias/Documents/My Files/Backup/GitHub/Public/MatLab/Image Processing/01 - Shading/Dataset/tungsten_filament_restored.tif")
+imwrite(result, "Dataset/tungsten_filament_restored.tif")
 
 % Será necessário fazer o reescalonamento dos valores após a aplicação da operação aritmética apropriada?
 % Não, pois a imagem resultante já possui valores entre 0 e 1, visto que ambas as imagens de entrada também possuem valores entre 0 e 1 graças ao uso da função im2double.
